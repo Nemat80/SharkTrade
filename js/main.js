39e5d8321061db077e1a7b5d4ba6c6d996a831e8
dars 1 <dasr1 1>
2023-07-07 15:35:24 +0500

    create slider

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 // burger menu
 function toggleBurger(item) {
-    item.classList.toggle("active");
-    let menu = document.querySelector('.header-menu')
-    menu.classList.toggle("open");
+  item.classList.toggle("active");
+  let menu = document.querySelector('.header-menu')
+  menu.classList.toggle("open");
 }
 
 
@@ -15,20 +15,20 @@ const tabsItems = document.querySelectorAll('.tabs-item')
 
 
 tabsBtn.forEach(item => {
-    item.addEventListener('click', () => {
-        let currentBtn = item;
-        let tabId = currentBtn.getAttribute('data-tab')
-        let currentTab = document.querySelector(tabId)
+  item.addEventListener('click', () => {
+    let currentBtn = item;
+    let tabId = currentBtn.getAttribute('data-tab')
+    let currentTab = document.querySelector(tabId)
 
-        tabsBtn.forEach((item) => {
-            item.classList.remove('activeted')
-        })
-        tabsItems.forEach((item) => {
-            item.classList.remove('show')
-        })
-        currentBtn.classList.add('activeted')
-        currentTab.classList.add('show')
+    tabsBtn.forEach((item) => {
+      item.classList.remove('activeted')
     })
+    tabsItems.forEach((item) => {
+      item.classList.remove('show')
+    })
+    currentBtn.classList.add('activeted')
+    currentTab.classList.add('show')
+  })
 })
 
 
@@ -37,10 +37,44 @@ tabsBtn.forEach(item => {
 
 
 function toggleAccordion(item) {
-    item.classList.toggle("active");
-    let content = item.lastElementChild;
-    content.classList.toggle("active");
+  item.classList.toggle("active");
+  let content = item.lastElementChild;
+  content.classList.toggle("active");
 }
+
+
+
+// SLIDER RESULT
+
+
+let rangeSlider = document.getElementById("rs-range-line");
+let rangeBullet = document.getElementById("rs-bullet");
+
+rangeSlider.addEventListener("input", showSliderValue, false);
+
+function showSliderValue() {
+  rangeBullet.innerHTML = rangeSlider.value;
+  let bulletPosition = (rangeSlider.value / rangeSlider.max);
+  rangeBullet.style.left = (bulletPosition * 560) + "px";
+}
+
+
+function updateSlider() {
+
+  let slider = document.querySelector("#rs-range-line");
+  let value = slider.value;
+
+  if (value >= 1500) {
+    slider.classList.add("mid");
+  }
+  else {
+    slider.classList.remove('mid');
+  }
+
+
+}
+
+
 
 
 
@@ -49,28 +83,28 @@ function toggleAccordion(item) {
 let prevScrollPos = window.pageYOffset;
 
 
-window.addEventListener("scroll", function() {
-    const currentScrollPos = window.pageYOffset;
+window.addEventListener("scroll", function () {
+  const currentScrollPos = window.pageYOffset;
 
-    if (prevScrollPos > currentScrollPos) {
+  if (prevScrollPos > currentScrollPos) {
 
-      document.querySelector('header').style.top = '0';
-    } else {
-      document.querySelector('header').style.top = '-100px';
-    }
-  
-    prevScrollPos = currentScrollPos;
-  
+    document.querySelector('header').style.top = '0';
+  } else {
+    document.querySelector('header').style.top = '-100px';
+  }
 
-    let header = document.querySelector("header");
-    let scrolled = window.pageYOffset || document.documentElement.scrollTop;
-    
-    if (scrolled > 0) {
-      header.classList.add("show");
-    } else {
-      header.classList.remove("show");
-      
-    }
-  });
-  
-  window.addEventListener('scroll', hideShowHeader);
+  prevScrollPos = currentScrollPos;
+
+
+  let header = document.querySelector("header");
+  let scrolled = window.pageYOffset || document.documentElement.scrollTop;
+
+  if (scrolled > 0) {
+    header.classList.add("show");
+  } else {
+    header.classList.remove("show");
+
+  }
+});
+
+window.addEventListener('scroll', hideShowHeader);
